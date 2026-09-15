c8126b3ddd3dece05594cdcf3b2b8ac126f37bac
MAGMA-KROSS <abik4002@gmail.com>
2026-09-15 22:49:20 +0530

    majority element problem

--- Arrays/169_majority_element_brute.cpp
+++ Arrays/169_majority_element_brute.cpp
@@ -8,6 +8,28 @@ using namespace std;
 int main()
 {
     vector<int> nums={2,2,1,1,1,2,2};
+    int count=0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int j = 0; j < nums.size(); j++)
+        {
+            if (nums[i]==nums[j])
+            {
+                count++;
+            }
+            
+        }
+        if (count>(nums.size()/2))
+        {
+            cout<< nums[i]<<" is the majority element";
+            break;
+        }
+        else
+        {
+            count = 0;
+        }
+        
+    }
     
     return 0;
 }
